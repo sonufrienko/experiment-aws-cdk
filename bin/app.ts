f55f7343c53a718c7bba743eb69e6d6aae4dd1dc
Sergey Onufrienko <sergey@onufrienko.com>
2019-12-19 19:24:38 +0200

    Add region to stack

--- bin/app.ts
+++ bin/app.ts
@@ -4,5 +4,5 @@ import HostingStack from '../lib/hosting-stack';
 import VpcStack from '../lib/vpc-stack';
 
 const app = new cdk.App();
-new HostingStack(app, 'HostingStack');
-new VpcStack(app, 'VpcStack');
+new HostingStack(app, 'HostingStack', { env: { 'region': 'eu-west-1' } });
+new VpcStack(app, 'VpcStack', { env: { 'region': 'eu-west-1' } });
