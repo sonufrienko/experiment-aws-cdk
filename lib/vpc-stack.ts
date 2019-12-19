6a2dc548262aa342d428e0f4699d663c4223fe52
Sergey Onufrienko <sergey@onufrienko.com>
2019-12-19 19:26:55 +0200

    Add SSH port

--- lib/vpc-stack.ts
+++ lib/vpc-stack.ts
@@ -1,10 +1,19 @@
 import cdk = require('@aws-cdk/core');
+import { Duration } from '@aws-cdk/core';
 import ec2 = require('@aws-cdk/aws-ec2');
+import autoscaling = require('@aws-cdk/aws-autoscaling');
+import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
+
+const SSH_KEY_NAME = 'my-eu-west-1';
 
 export default class VpcStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+
+    /**
+     * VPC
+     */
     const vpc = new ec2.Vpc(this, 'VPC', {
       maxAzs: 2,
       subnetConfiguration: [
@@ -20,13 +29,20 @@ export default class VpcStack extends cdk.Stack {
       ]
     });
 
+
+    /**
+     * Security Group
+     */
     const webSecurityGroup = new ec2.SecurityGroup(this, 'WebSG', {
       vpc,
-      description: 'Allow http(s) traffic',
+      description: 'Allow public access',
       allowAllOutbound: true
     });
 
     webSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'Allow http');
     webSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), 'Allow https');
+    webSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'SSH');
+
+
   }
 }
